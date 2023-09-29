a071ef0f17b4fd072a12828736adf7186fb67a49
Caioxz <112172836+Caioxz@users.noreply.github.com>
2023-09-29 20:56:48 -0300

    criação do projeto

---
+++ LoginGithub.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+
+pyautogui.press('win')
+time.sleep(0.70)
+pyautogui.write('chrome')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.write('https://github.com/')
+time.sleep(1.3)
+pyautogui.press('enter')
+time.sleep(1.5)
+pyautogui.click(x=1217, y=115)#clicar no "entrar" do github
+time.sleep(1.8)
+pyautogui.click(x=700, y=289)#clicar no email
+time.sleep(1)
+pyautogui.write('caio.pinheiro15@etec.sp.gov.br')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.write('CSP123@241f')
+time.sleep(0.90)
+pyautogui.press('tab')
+pyautogui.press('enter')
